4cf20a33c19d208671a473db6c7e94a7909146b9
Soroush Borzoo <94343205+SoVaBo@users.noreply.github.com>
2021-12-28 08:03:15 +0000

    add data frame

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import streamlit as st
+import pandas
+
+data = {
+  "series_1":[1,3,4,5,7],
+  "series_2":[10,30,40,100,250]
+}
+df=pandas.DataFrame(data)
 
 st.title("First Webb App")
 st.subheader("it is going to be awsome")
 st.write('''just wait for it''')
+st.write(df)
+
