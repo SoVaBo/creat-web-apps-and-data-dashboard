740286e9ea2889e2e4f26f0849b41e91c7450cef
Soroush Borzoo <94343205+SoVaBo@users.noreply.github.com>
2022-03-09 07:15:02 +0000

    Design the web page contents

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 import streamlit as st
 import pandas
+#design the web app
 
-data = {
-  "series_1":[1,3,4,5,7],
-  "series_2":[10,30,40,100,250]
-}
-df=pandas.DataFrame(data)
+st.title("Mood from Speech")
+st.subheader("We analyze your audio file based on words you use and say if it is positive or not!")
+st.write('Please upload your Audio file:')
 
-st.title("First Webb App")
-st.subheader("it is going to be awsome")
-st.write('''just wait for it''')
-st.write(df)
+#upload and save Audio file
 
-st.line_chart(df)
-st.area_chart(df)
+#turn to text
 
-myslider=st.slider("Pound")
-st.write(myslider,"in Pounds is",myslider*0.453592,"in Kgs")
+#analyzing the text
+#show the result
