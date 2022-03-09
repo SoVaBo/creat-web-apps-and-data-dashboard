aa33e6c16cb15373eecd83b62ca80baccbb1cdc2
Soroush Borzoo <94343205+SoVaBo@users.noreply.github.com>
2022-03-09 07:39:18 +0000

    upload the audio file

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import streamlit as st
 import pandas
+from pydub import AudioSegment
+from pathlib import Path
+from speech_recognition import Recognizer, AudioFile
 #design the web app
 
 st.title("Mood from Speech")
 st.subheader("We analyze your audio file based on words you use and say if it is positive or not!")
-st.write('Please upload your Audio file:')
+
 
 #upload and save Audio file
+uploaded_file = st.file_uploader("Please upload your Audio file:",type=['wav'])
+if uploaded_file is not None:
+  if uploaded_file.name.endswith('wav'):
+    audio = pydub.AudioSegment.from_wav(uploaded_file)
+    
 
 #turn to text
 
